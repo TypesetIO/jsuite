8c31d0739147a25a339e94d71593245858f97b81
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2016-11-20 18:48:29 +0530

    Add dependencies in the setup.py file

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from distutils.core import setup
 setup(
   name = 'jsuite',
   packages = ['jsuite'], # this must be the same as the name above
+  version = '0.1.3',
+
   scripts=['bin/jsuite'],
-  version = '0.1.1',
+  install_requires = [
+    'lxml==3.6.4',
+    'clint==0.5.1'
+  ],
+
   description = 'Parsing and manipulation tools for JATS XML files.',
   author = 'Dipanjan Mukherjee',
   author_email = 'dipanjan.mu@gmail.com',
