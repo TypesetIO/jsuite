3380126f0d99173ba16ef56d3b9e2d39c299724c
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2017-03-13 18:25:37 +0530

    Add a test to verify title extraction for unicode strings

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from .context import jsuite, fixture
 
 import unittest
@@ -8,3 +9,9 @@ class ExtractSuite(unittest.TestCase):
       doc = jsuite.from_file(f)
       title = jsuite.extract.extract_title(doc)
       assert(title == 'Sample article title')
+
+  def test_article_title_unicode(self):
+    with fixture('extract/title-with-unicodes') as f:
+        doc = jsuite.from_file(f)
+        title = jsuite.extract.extract_title(doc)
+        assert(title == u'Sample article title † ecolé Straßße')
