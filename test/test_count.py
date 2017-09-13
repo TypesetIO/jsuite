98b5336db5e97afcd94fbc679dfbe27f88a3f6d8
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2017-09-13 21:53:29 +0530

    fix test: There are 5 figures in the 4-figures file
    One of the figures is in the front matter.

--- test/test_count.py
+++ test/test_count.py
@@ -14,4 +14,4 @@ class CountSuite(unittest.TestCase):
     with fixture('count/4-figures') as f:
       doc = jsuite.from_file(f)
       count = jsuite.count.of_type(doc, 'fig')
-      assert(count == 4)
+      assert(count == 5)
